0a4c9bdd40f8232beb813255af957ad1d38ebbd0
Niall Quinlan <niallquinlan@gmail.com>
2025-02-07 15:42:07 +0000

    mouse mixin

--- src/client/java/ninja/trek/mixin/client/MouseMixin.java
+++ src/client/java/ninja/trek/mixin/client/MouseMixin.java
@@ -1,16 +1,23 @@
 package ninja.trek.mixin.client;
 
 import net.minecraft.client.Mouse;
+import ninja.trek.CraneshotClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import ninja.trek.CraneshotClient;
 
 @Mixin(Mouse.class)
 public class MouseMixin {
-    @Shadow private double eventDeltaVerticalWheel;
+    @Shadow
+    private double eventDeltaVerticalWheel;
+    @Shadow private double cursorDeltaX;
+    @Shadow private double cursorDeltaY;
+
+    private boolean interceptMouse = false;
+    private double lastDeltaX;
+    private double lastDeltaY;
 
     @Inject(method = "onMouseScroll", at = @At("HEAD"), cancellable = true)
     private void onScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
@@ -22,5 +29,26 @@ public class MouseMixin {
             eventDeltaVerticalWheel = vertical;
             ci.cancel();
         }
+    }
+
+    @Inject(method = "updateMouse", at = @At("HEAD"), cancellable = true)
+    private void onUpdateMouse(double timeDelta, CallbackInfo ci) {
+        if (interceptMouse) {
+            lastDeltaX = cursorDeltaX;
+            lastDeltaY = cursorDeltaY;
+            ci.cancel();
+        }
+    }
+
+    public void setInterceptMouse(boolean intercept) {
+        this.interceptMouse = intercept;
+    }
+
+    public double getLastDeltaX() {
+        return lastDeltaX;
+    }
+
+    public double getLastDeltaY() {
+        return lastDeltaY;
     }
 }
